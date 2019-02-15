ff379994e26f13a4a7c967c671f99b7df0ecd7fe
unknown <holdcroftkari@gmail.com>
2019-02-15 16:30:44 +0100

    completed assignment

--- level1/question1/script.js
+++ level1/question1/script.js
@@ -1,1 +1,30 @@
 //Use RegEx to validate form
+var contactForm = document.forms["contact"];
+
+contactForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    var formElements = contactForm.elements;
+    var numberOfElements = formElements.length;
+
+    for (var index = 0; index < numberOfElements; index++) {
+
+        var element = formElements[index];
+
+        if (element.pattern !== "") {
+
+            var validationPattern = new RegExp(element.pattern);
+            var elementValue = element.value;
+            var validationTest = validationPattern.test(elementValue);
+            var labelText = element.labels[0].innerText;
+
+            if (element.type === "text" && elementValue === "") {
+                console.log(labelText + " can't be empty");
+            } else {
+                if (validationTest === false) {
+                    console.log(labelText + " format is invalid")
+                }
+            }
+        }
+    }
+});
--- level1/question2/script.js
+++ level1/question2/script.js
@@ -1,1 +1,34 @@
 //Console.log out elements in JSON file
+var author = {
+    "video": [
+        {
+            "id": 12312412312,
+            "name": "Ecuaciones Diferenciales",
+            "url": "/video/math/edo/12312412312",
+            "author": {
+                "data": [
+                    {
+                        "name_author": "Alejandro Morales",
+                        "url": "/author/alejandro-morales",
+                        "type": "master"
+                    }
+                ]
+            }
+        }
+    ]
+};
+
+var video = author.video[0];
+var authorData = video.author.data[0];
+
+console.log(video.id);
+console.log(video.name);
+console.log(video.url);
+console.log(video.author);
+
+for (var data in authorData) {
+    if (authorData.hasOwnProperty(data)) {
+        var value = authorData[data];
+        console.log(value);
+    }
+}
